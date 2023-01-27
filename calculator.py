5a98a3e61db24a62a8ded7f2c454fa4a8e440067
VIBHAV19 <vpissurlenkar23@gmail.com>
2023-01-27 11:30:46 +0530

    Added add function

---
+++ calculator.py
@@ -0,0 +1,22 @@
+class cal():
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+    def add(self):
+        return self.a + self.b
+
+a = int(input('Enter First number : '))
+b = int(input('Enter Second number : '))        
+obj=cal(a,b)
+while True:    
+    def menu():
+        x = ('1. Add \n') 
+        print(x)
+    menu()
+    choice = int(input('Please select one of the following : ')) 
+    if choice == 1:
+        print("Result: ",obj.add())
+    else:
+        print('Invalid option') 
+        break       
+print()
