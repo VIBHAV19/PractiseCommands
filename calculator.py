b23f41997847f0b40cea7a66425d66d0a73d7d81
VIBHAV19 <vpissurlenkar23@gmail.com>
2023-01-27 11:44:10 +0530

    final calculator commit

--- calculator.py
+++ calculator.py
@@ -8,13 +8,15 @@ class cal():
         return self.a - self.b
     def multiply(self):
         return self.a * self.b
+    def divide(self):
+        return self.a / self.b
 
 a = int(input('Enter First number : '))
 b = int(input('Enter Second number : '))        
 obj=cal(a,b)
 while True:    
     def menu():
-        x = ('1. Add \n2. Sub \n3. Multiply \n') 
+        x = ('1. Add \n2. Sub \n3. Multiply \n 4. Divide \n') 
         print(x)
     menu()
     choice = int(input('Please select one of the following : ')) 
@@ -24,6 +26,11 @@ while True:
         print("Result: ",obj.sub())
     elif choice == 3:
         print("Result: ",obj.multiply())
+    elif choice == 4:
+        print("Result: ",obj.divide())
+    elif choice == 0:
+        print('Again try one of the following')
+        break
     else:
         print('Invalid option') 
         break       
