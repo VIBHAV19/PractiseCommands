0b4118f06d37eeb1e5554b28d67d81b1774325e3
VIBHAV19 <vpissurlenkar23@gmail.com>
2023-01-27 11:35:26 +0530

    added subtract function

--- calculator.py
+++ calculator.py
@@ -4,18 +4,22 @@ class cal():
         self.b = b
     def add(self):
         return self.a + self.b
+    def sub(self):
+        return self.a - self.b
 
 a = int(input('Enter First number : '))
 b = int(input('Enter Second number : '))        
 obj=cal(a,b)
 while True:    
     def menu():
-        x = ('1. Add \n') 
+        x = ('1. Add  \n2. Sub \n') 
         print(x)
     menu()
     choice = int(input('Please select one of the following : ')) 
     if choice == 1:
         print("Result: ",obj.add())
+    elif choice == 2:
+        print("Result: ",obj.sub())
     else:
         print('Invalid option') 
         break       
